ff597f09ec29565b3dc5275f41b8c852c83f47b3
Samuel Li <sam@projreality.com>
2012-08-10 12:32:59 -0700

    Add File/Open option.
    Added function to handle opening a new file (either through File/New or File/Open)

--- QSLife.py
+++ QSLife.py
@@ -9,7 +9,7 @@ import init;
 class QSLife(wx.Frame):
 
 ################################################################################
-################################ CONSTRUCTOR ###################################
+################################## CONSTRUCTOR #################################
 ################################################################################
   def __init__(self, *args, **kwargs):
     super(QSLife, self).__init__(*args, **kwargs);
@@ -22,27 +22,29 @@ class QSLife(wx.Frame):
     self.gui_miscellaneous_setup();
 
 ################################################################################
-############################### CREATE MENUBAR #################################
+################################ CREATE MENUBAR ################################
 ################################################################################
   def create_menubar(self):
     menubar = wx.MenuBar();
 
-################################# FILE MENU ####################################
+################################### FILE MENU ##################################
     # Structure
     menu_file = wx.Menu();
     menu_file_new = menu_file.AppendItem(wx.MenuItem(menu_file, wx.ID_NEW, "&New\tCtrl+N"));
+    menu_file_open = menu_file.AppendItem(wx.MenuItem(menu_file, wx.ID_OPEN, "&Open\tCtrl+O"));
     menu_file_exit = menu_file.AppendItem(wx.MenuItem(menu_file, wx.ID_EXIT, "E&xit\tCtrl+Q"));
     menubar.Append(menu_file, "&File");
 
     # Events
     self.Bind(wx.EVT_MENU, self.onFileNew, menu_file_new);
+    self.Bind(wx.EVT_MENU, self.onFileOpen, menu_file_open);
     self.Bind(wx.EVT_MENU, self.onFileExit, menu_file_exit);
 
-############################## FINISH MENUBAR ##################################
+################################ FINISH MENUBAR ################################
     self.SetMenuBar(menubar);
 
 ################################################################################
-########################## GUI MISCELLANEOUS SETUP #############################
+############################ GUI MISCELLANEOUS SETUP ###########################
 ################################################################################
   def gui_miscellaneous_setup(self):
     self.statusbar = self.CreateStatusBar();
@@ -51,21 +53,38 @@ class QSLife(wx.Frame):
     self.Show();
 
 ################################################################################
-############################### EVENT HANDLERS #################################
+################################### OPEN FILE ##################################
+################################################################################
+  def open_file(self, path):
+    self.current_file = path;
+    self.SetTitle(path);
+
+################################################################################
+################################# EVENT HANDLERS ###############################
 ################################################################################
 
-################################### FILE NEW ###################################
+#################################### FILE NEW ##################################
   def onFileNew(self, e):
-    dialog = wx.FileDialog(None, "New HDFQS file...", ".", style=wx.FD_SAVE);
+    dialog = wx.FileDialog(None, "New HDFQS file ...", ".", style=wx.FD_SAVE);
     if (dialog.ShowModal() == wx.ID_OK):
       path = dialog.GetPath();
       if (os.path.exists(path)):
 	dialog = wx.MessageDialog(None, "File \"" + path + "\" exists - overwrite?", "Confirm", wx.YES_NO);
 	if (dialog.ShowModal() == wx.ID_YES):
 	  init.init(path);
-	  self.current_file = path;
-	  self.SetTitle(path);
+	  self.open_file(path);
 	  self.statusbar.SetStatusText("Created new file \"" + path + "\"");
+
+################################### FILE OPEN ##################################
+  def onFileOpen(self, e):
+    dialog = wx.FileDialog(None, "Open HDFQS file ...", ".", style=wx.FD_OPEN);
+    if (dialog.ShowModal() == wx.ID_OK):
+      path = dialog.GetPath();
+      if (os.path.exists(path)):
+	self.open_file(path);
+	self.SetStatusText("Opened file \"" + path + "\"");
+      else:
+	wx.MessageBox("File \"" + path + "\" does not exist", "Error", wx.OK | wx.ICON_EXCLAMATION);
 
 ################################### FILE EXIT ##################################
   def onFileExit(self, e):
