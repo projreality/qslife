04c3d20f4a42419b9da712fd5a700ea706105262
Samuel Li <sam@projreality.com>
2012-08-12 19:12:49 -0700

    Load configuration from file.

--- QSLife.py
+++ QSLife.py
@@ -44,12 +44,14 @@ class QSLife(wx.Frame):
     # Structure
     menu_file = wx.Menu();
     menu_file_new = menu_file.AppendItem(wx.MenuItem(menu_file, wx.ID_NEW, "&New\tCtrl+N"));
+    menu_file_open = menu_file.AppendItem(wx.MenuItem(menu_file, wx.ID_OPEN, "&Open\tCtrl+O"));
     menu_file_load = menu_file.AppendItem(wx.MenuItem(menu_file, wx.ID_ANY, "&Load HDFQS file\tCtrl+L"));
     menu_file_exit = menu_file.AppendItem(wx.MenuItem(menu_file, wx.ID_EXIT, "E&xit\tCtrl+Q"));
     menubar.Append(menu_file, "&File");
 
     # Events
     self.Bind(wx.EVT_MENU, self.onFileNew, menu_file_new);
+    self.Bind(wx.EVT_MENU, self.onFileOpen, menu_file_open);
     self.Bind(wx.EVT_MENU, self.onFileLoad, menu_file_load);
     self.Bind(wx.EVT_MENU, self.onFileExit, menu_file_exit);
 
@@ -82,6 +84,21 @@ class QSLife(wx.Frame):
     self.SetTitle("-");
     self.Maximize();
     self.Show();
+
+################################################################################
+################################## OPEN CONFIG #################################
+################################################################################
+  def open_config(self, path):
+
+    temp = { };
+    execfile(path, temp);
+    self.current_file = temp["current_file"];
+    self.load_file(self.current_file);
+    self.graphs.set_current_file(self.current_file);
+    self.graphs.set_time_range(temp["time_range"]);
+    self.graphs.set_graphs(temp["graph_config"]);
+    self.graphs.update();
+
 
 ################################################################################
 ################################### LOAD FILE ##################################
@@ -120,6 +137,17 @@ class QSLife(wx.Frame):
 	  init.init(path);
 	  self.load_file(path);
 	  self.statusbar.SetStatusText("Created new file \"" + path + "\"");
+
+################################### FILE OPEN ##################################
+  def onFileOpen(self, e):
+    dialog = wx.FileDialog(None, "Open QSLife configuration file...", ".", style=wx.FD_OPEN, wildcard="*.py");
+    if (dialog.ShowModal() == wx.ID_OK):
+      path = dialog.GetPath();
+      if (os.path.exists(path)):
+	self.open_config(path);
+	self.SetStatusText("Opened configuration file \"" + path + "\"");
+      else:
+	wx.MessageBox("File \"" + path + "\" does not exist", "Error", wx.OK | wx.ICON_EXCLAMATION);
 
 ################################### FILE OPEN ##################################
   def onFileLoad(self, e):
